f5b19e9a5eff5dc33809175226f727ff79db57f0
salehaTanveer <56721766+salehaTanveer@users.noreply.github.com>
2020-06-21 16:55:30 +0500

    Update GlobalState.js

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,2 +1,31 @@
 import React , {createContext , userReducer } from 'react';
 
+
+//initial state
+const initialState = {
+    transactions: [
+        { id: 1, description: "Project 1 Income", transactionAmount: 10000 },
+        { id: 2, description: "Project 2 Income", transactionAmount: 20000 },
+        { id: 3, description: "Project 1 Salary", transactionAmount: -5000 },
+        { id: 4, description: "Project 2 Salary", transactionAmount: -10000 }
+    ]
+}
+
+//Global Context 
+export const GlobalContext = createContext(initialState);
+
+//provider for Global Context 
+export const GlobalProvider = ({ children }) => {
+    const [ state, dispath ] = userReducer(AppReducer , initialState);
+
+    return(
+        <GlobalContext.Provider value= {
+            {
+            transactions: state.transactions
+            }
+        }>
+            {children}
+        </GlobalContext.Provider>
+    )
+
+}
